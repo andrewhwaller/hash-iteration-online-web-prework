16bd7af0d6655bf9abe8a7194067a9ba97e7689c
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 00:37:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,6 +5,6 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do { |kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old!" }
+  birthday_kids.each do |kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
